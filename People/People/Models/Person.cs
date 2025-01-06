f203649fe81ac97f8057daea028951e1b98910a1
ptrickill <daniel.diaz.mosquera8@gmail.com>
2025-01-06 08:44:53 -0500

    1 commit

--- People/People/Models/Person.cs
+++ People/People/Models/Person.cs
@@ -1,4 +1,12 @@
 ﻿namespace People.Models;
+using SQLite;
+
+[Table("People")]
 public class Person
 {
+    [PrimaryKey]
+    public int Id { get; set; }
+    [MaxLength(250), Unique]
+    public string Name { get; set; }
+
 }
